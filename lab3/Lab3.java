e4d3aa73f879a5a2a8a4fda0d519abf7690180eb
sunghwan1234 <sunghwan1002222@gmail.com>
2025-10-15 11:14:18 -0500

    lab3 out of spite

--- lab3/Lab3.java
+++ lab3/Lab3.java
@@ -1,33 +1,31 @@
 import java.util.Scanner; // Sunghwan In
 import java.lang.Math;
 
-public class Main {
+public class Lab3 {
     public static void main(String[] args) {
       Scanner scanner = new Scanner(System.in);
       int[] data = {-1,0,0,0,0,0,0}; // choice,cchoice,win,loss,spartans,panthers,rockets
       String[] teams = {"","Spartans","Panthers","Rockets"}; // Spartans > Panthers > Rockets > Spartans
-      String winner = "No one";
       while (data[2]<10 && data[3]<10) {
         data[0] = -1;
         data[1] = (int)(Math.random()*3)+1;
-        data[data[1]+3]++;
-        while (data[0]<0 || data[0]>3) {
-          System.out.println("1 for Spartans, 2 for Panthers, 3 for Rockets, 0 to exit: ");
+        data[data[1]+3]++; // funny intermixing
+        while (data[0]<0 || data[0]>3) { // Input validation, of course
+          System.out.println("1 for Spartans, 2 for Panthers, 3 for Rockets, 0 to exit: "); // Allows exiting
           data[0] = scanner.nextInt();
         }
-        if (data[0]==0) {break;}
-        winner = (data[0]==data[1]?"No one":data[0]<data[1]||data[0]==3&&data[1]==1?"You":"I");
+        if (data[0]==0) {
+          break;
+        } // Allows exiting
         String line = "You chose [a]. I chose [b].\n[c] won this time. You have [d] wins, I have [e] wins.[f]\nComputer Choices: ";
-        for (int i=0;i<data[4]+data[5]+data[6];i++) {
-          line += teams[(i<data[4]?1:i<data[4]+data[5]?2:3)]+" ";
+        for (int i=0;i<data[4]+data[5]+data[6];i++) { // amazing 'for' loop to iterate through all computer choices
+          line += teams[(i<data[4]?1:i<data[4]+data[5]?2:3)]+" "; // advanced selection system using one variable
         }
-        System.out.println(line.substring(0,line.indexOf("[a]"))+teams[data[0]]+line.substring(line.indexOf("[a]")+3,line.indexOf("[b]"))+teams[data[1]]+line.substring(line.indexOf("[b]")+3,line.indexOf("[c]"))+winner+line.substring(line.indexOf("[c]")+3,line.indexOf("[d]"))+(winner=="You"?++data[2]:data[2])+line.substring(line.indexOf("[d]")+3,line.indexOf("[e]"))+(winner=="I"?++data[3]:data[3])+line.substring(line.indexOf("[e]")+3,line.indexOf("[f]"))+(data[2]==10?"\nYou won!":data[3]==10?"\nI won!":"")+line.substring(line.indexOf("[f]")+3,line.length()-1)
-        );
+        System.out.println(line.substring(0,line.indexOf("[a]"))+teams[data[0]]+line.substring(line.indexOf("[a]")+3,line.indexOf("[b]"))+teams[data[1]]+line.substring(line.indexOf("[b]")+3,line.indexOf("[c]"))+(data[0]==data[1]?"No one":data[0]<data[1]||data[0]==3&&data[1]==1?"You":"I")+line.substring(line.indexOf("[c]")+3,line.indexOf("[d]"))+((data[0]==data[1]?"No one":data[0]<data[1]||data[0]==3&&data[1]==1?"You":"I")=="You"?++data[2]:data[2])+line.substring(line.indexOf("[d]")+3,line.indexOf("[e]"))+((data[0]==data[1]?"No one":data[0]<data[1]||data[0]==3&&data[1]==1?"You":"I")=="I"?++data[3]:data[3])+line.substring(line.indexOf("[e]")+3,line.indexOf("[f]"))+(data[2]==10?"\nYou won!":data[3]==10?"\nI won!":"")+line.substring(line.indexOf("[f]")+3,line.length()-1));
       }
       System.out.println("\nClosing.");
-      scanner.close();
     }
-    public static double dist(double[] p1,double[] p2) {
+    public static double dist(double[] p1,double[] p2) { // useless funcitons
       return Math.sqrt(Math.pow(p2[0]-p1[0],2)+Math.pow(p2[1]-p1[1],2));
     }
     public static void pl(String p) {
