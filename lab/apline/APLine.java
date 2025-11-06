5353706fae0641216b2d99a8550d62901500c6f2
Sunghwan <147672384+Sunghwan1234@users.noreply.github.com>
2025-11-06 19:33:36 +0000

    apline

--- lab/apline/APLine.java
+++ lab/apline/APLine.java
@@ -1,5 +1,14 @@
 package lab.apline;
 
 public class APLine {
-    
+    double a,b,c;
+    public APLine(double ia, double ib, double ic) {
+        a=ia; b=ib; c=ic;
+    }
+    public double getSlope() {
+        return -a/b;
+    }
+    public boolean isOnLine(double x, double y) {
+        return a*x + b*y==-c;
+    }
 }
