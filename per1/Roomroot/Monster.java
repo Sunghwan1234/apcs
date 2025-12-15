1205e46deda24c7f858fdb3ea693a23f4f7f9a80
sunghwan1234 <sunghwan1002222@gmail.com>
2025-12-15 11:15:29 -0600

    roomroot7.6

--- per1/Roomroot/Monster.java
+++ per1/Roomroot/Monster.java
@@ -10,43 +10,36 @@ public class Monster implements Entity {
     public int chance;
     public Monster(Player player) {
         Monster[] monsters = {
-            new Monster("Goblin",3),
-            new Monster("Bully Goblin", 5),
-            new Monster("Troll", 7),
-            new Monster("Orc", 10)
+            new Monster("Goblin", 3, 10, 0, 5),
+            new Monster("Bully Goblin", 5, 10, 0 ,5),
+            new Monster("Troll", 7, 10, 0, 5),
+            new Monster("Orc", 10, 100, 0, 25)
         };
 
         while (type==null) {
             Monster m = monsters[(int)(Math.random()*monsters.length)];
-            if (Math.random()*(player.level+10) > m.level) {type=m.name;}
+            if (Math.random()*(player.level+10) > m.level) {
+                create(m);
+            }
         }
-
     }
-
-    public Monster(String type) {
-        this.name = type;
-        switch (type) {
-            case "Goblin":      c_basic(10,0,5); break;
-            case "Bully Goblin":c_basic(25,0,10); break;
-            case "Troll":       c_basic(50,0,15); break;
-            case "Orc":         c_basic(100,0,25); break;
-            default: break;
-        }
-    };
-
-    public Monster(String type, int level) {
+    public Monster(String type, int level, int hp, int mana, int damage) {
+        c_basic(type,level, hp, mana, damage);
+    }
+    public void c_basic(String type, int level, int hp, int mana, int damage) {
         this.type=type;
         this.level=level;
+        this.hp=hp;
+        this.mana=mana;
+        this.damage=damage;
     }
-
-    private void c_basic(int hp, int mana, int damage) {
-        this.hp_max = hp;
-        this.hp = hp;
-        this.mana_max = mana;
-        this.mana = mana;
-        this.damage = damage;
+    public void create(Monster m) {
+        this.type=m.type;
+        this.level=m.level;
+        this.hp=m.hp;
+        this.mana=m.mana;
+        this.damage=m.damage;
     }
-
 
     @Override
     public ArrayList<Action> getActions() {
