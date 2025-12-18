46b8ffb20f98f511aa0d54ac1af52ba251df51a8
sunghwan1234 <sunghwan1002222@gmail.com>
2025-12-18 11:17:24 -0600

    lab6

---
+++ lab/lab6/AoC.java
@@ -0,0 +1,24 @@
+package lab6;
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class AoC {
+  public static void main(String[] args) throws FileNotFoundException {
+    File textFile = new File("lab/lab6/input.txt");
+    Scanner userInput = new Scanner(textFile);
+    int[] depths = new int[2000];
+    for (int i = 0; i < depths.length; i++) {
+      depths[i] = userInput.nextInt();
+    }
+
+    int c=0;
+    
+    for (int i=1;i<depths.length;i++) {
+      if (depths[i-1]>depths[i]) {c++;}
+    }
+    System.out.print(c);
+  }
+}
+
---
+++ lab/lab6/ReactionTimes.java
@@ -0,0 +1,42 @@
+package lab6;
+
+import java.util.Scanner;
+
+/**
+ * ReactionTimes contains a main method to construct an array to hold the reaction
+ * times of the user in a game.  The method finds the average of these reaction
+ * times and prints it out.
+ */
+public class ReactionTimes {
+  public static void main(String[] args) {
+    // 1. Create a Scanner object.
+    
+    
+    // 2. Declare and instantiate an array called "times" that contains 5 double elements.
+    //    Don't initialize any values yet.
+    
+    
+    // 3. Create a for loop that will fill the times array with values.
+    //    Each time the for loop is iterated, ask the user for their reaction time.
+    
+    
+    // 4. Create a double variable called firstAverage that will contain the average reaction time of the user.
+    //    Use a for each loop to calculate and store firstAverage.
+    
+    
+    // 5. Print a message stating the first average reaction times of the user.
+    
+                       
+    // 6. Create a for loop that will fill the times array with new values.
+    //    Each time the for loop is iterated, ask the user for their reaction time.
+    
+        
+    // 7. Create a double variable called secondAverage that will contain the new average reaction time of the user.
+    //    Use a for-each loop to calculate and store secondAverage.
+    
+    
+    // 8. Print a message stating the second average reaction times of the user.
+    
+    
+  }
+}
