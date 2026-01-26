36ae75a1256c8f62e590d0f9037f328da07d6e55
sunghwan1234 <sunghwan1002222@gmail.com>
2026-01-26 11:16:01 -0600

    More progress on SortVisual

--- per1/SortVisualizer/Main.java
+++ per1/SortVisualizer/Main.java
@@ -2,7 +2,9 @@ package SortVisualizer;
 
 import javax.swing.*;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.MouseInfo;
@@ -16,39 +18,34 @@ public class Main {
   public static final int FRAME_WIDTH = 500, FRAME_HEIGHT = 500;
 
   public static JFrame frame = new JFrame("JFrame");
-  public static JPanel panel;
-  public static Timer timer;
-  public static ActionListener actionListener;
+  public static JPanel panel = new JPanel() {
+      @Override
+      public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.LIGHT_GRAY); g.fillRect(0, 0, FRAME_WIDTH, FRAME_HEIGHT);
+        
+        g.setColor(Color.black);
+        g.drawRect(mouse.x, mouse.y, 10, 10);
+      }
+    };
+  public static Timer timer = new Timer(1, new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        mouse = panel.getMousePosition()==null ? new Point(mouse) : panel.getMousePosition();
+        panel.repaint();
+      }
+    });;
 
   public static Point mouse = new Point(0, 10);
+  public static JButton button = new JButton("Run");
 
   public static int[] array = {4,3,7,8,2,9,1,5,6,0};
 
   public static void main(String[] args) {
     frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    panel.setLayout(new BorderLayout());
 
-    actionListener = new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        mouse = panel.getMousePosition()==null ? new Point(mouse) : panel.getMousePosition();
-        panel.repaint();
-      }
-    };
-
-    panel = new JPanel() {
-      // @Override
-      // public void paintComponent(Graphics g) {
-      //   //g.setColor(Color.LIGHT_GRAY); g.fillRect(0, 0, FRAME_WIDTH, FRAME_HEIGHT);
-        
-      //   g.setColor(Color.black);
-      //   //g.drawRect(mouse.x, mouse.y, 10, 10);
-
-      //   g.dispose();
-      // }
-    };
-
-    JButton button = new JButton("Click Me!");
     button.addActionListener(new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
@@ -56,11 +53,16 @@ public class Main {
         }
     });
 
-    timer = new Timer(1, actionListener);
-    // End of initialization.
-    panel.add(button);
+    JPanel controlPanel = new JPanel();
+    controlPanel.add(button);
+
+    panel.add(controlPanel, BorderLayout.SOUTH);
+
+    panel.setPreferredSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
+
     frame.add(panel);
     frame.setVisible(true);
+
     timer.start();
   }
 
