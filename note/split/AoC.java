cab5a0aae0be7de43d09882040b73399c2a95032
Sunghwan-apcs9-AR <sunghwan1002222@gmail.com>
2026-04-23 10:30:16 -0500

    AoC Updated

--- note/split/AoC.java
+++ note/split/AoC.java
@@ -16,15 +16,23 @@ public class AoC {
       String[] split = scanner.next().split("x");
       int[] space = new int[3];
       int[] areas = new int[space.length];
+      int[] perims = new int[space.length];
+      int smallestPerim=Integer.MAX_VALUE;
       for (int i=0;i<space.length;i++) {
         space[i]=Integer.parseInt(split[i]);
       }
       for (int i=0;i<areas.length;i++) {
         areas[i] = space[i]*space[(i+1)%3];
       }
+      for (int i=0;i<space.length;i++) {
+        perims[i] = 2*space[i]+2*space[(i+1)%3];
+        smallestPerim = perims[i]<smallestPerim?perims[i]:smallestPerim;
+      }
       int surfaceArea = 2*areas[0]+2*areas[1]+2*areas[2];
       int lowestArea = Math.min(Math.min(areas[0],areas[1]),areas[2]);
-      paper+=surfaceArea+lowestArea;
+      int ribbon = smallestPerim+space[0]*space[1]*space[2];
+      System.out.println(Arrays.toString(split)+" -> "+ribbon);
+      paper+=ribbon;
     }
     System.out.println(paper);
     scanner.close();
